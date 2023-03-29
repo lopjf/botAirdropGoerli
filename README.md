5d23798431f1e84e3ef21ea2bd55bead836f6648
loris <loris.penelon@outlook.com>
2023-03-29 10:04:31 +0100

    ok

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
-# airdrops
+# botAirdropGoerli
+
+This repository provides an example of an airdrop bot, which can only be executed through Netlify. However, executing these functions will not entitle you to any airdrops as they are intended solely for demonstration purposes.
+For an updated and functional airdrop bot, please reach out to me at lorispjf@gmail.com.
--- netlify/functions/transfer.js
+++ netlify/functions/transfer.js
@@ -10,12 +10,10 @@ const handler = async function (event, context) {
 
   const ACCOUNT_1 = new ethers.Wallet(process.env.PRIVATE_KEY_1, provider)
   const ACCOUNT_2 = new ethers.Wallet(process.env.PRIVATE_KEY_2, provider)
-  // const ACCOUNT_3 = new ethers.Wallet(process.env.PRIVATE_KEY_3, provider)
 
-  const ERC20 = new ethers.Contract(process.env.TOKEN_ADDRESS, IERC20.abi)
+  const USDCGoerli = new ethers.Contract(process.env.TOKEN_ADDRESS, IERC20.abi)
 
-  await ERC20.connect(ACCOUNT_1).transfer(ACCOUNT_2.address, process.env.TRANSFER_AMOUNT)
-  // await ERC20.connect(ACCOUNT_1).transfer(ACCOUNT_3.address, process.env.TRANSFER_AMOUNT)
+  await USDCGoerli.connect(ACCOUNT_1).transfer(ACCOUNT_2.address, process.env.TRANSFER_AMOUNT)
 
   console.log(`Transfers Complete!`)
   console.log(`See transactions at: https://goerli.etherscan.io/address/${ACCOUNT_1.address}`)
